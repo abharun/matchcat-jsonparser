cbd053d18e5e0c05d6cef45125019d2cfbde9b46
ABH <155776685+abharun@users.noreply.github.com>
2024-09-06 20:02:34 +0200

    feat: add album view board

---
+++ src/components/cards/AlbumCard.tsx
@@ -0,0 +1,24 @@
+import { AlbumInfo } from "@/types";
+import { NextPage } from "next";
+import InfoSpan from "@/components/InfoSpan";
+
+interface AlbumCardProps {
+  albumInfo: AlbumInfo;
+}
+
+const AlbumCard: NextPage<AlbumCardProps> = (props) => {
+  const { albumInfo } = props;
+  return (
+    <div className="flex w-full h-full border-2">
+      <div className="flex-col w-full h-full">
+        <div className="w-full h-full p-2">
+          <InfoSpan tag="ID" value={albumInfo.id} />
+          <InfoSpan tag="UserID" value={albumInfo.userId} />
+          <InfoSpan tag="Title" value={albumInfo.title} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlbumCard;
