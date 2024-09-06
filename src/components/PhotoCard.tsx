946e673957e039ea31eb78694292c4fc80017230
ABH <155776685+abharun@users.noreply.github.com>
2024-09-06 18:50:32 +0200

    fix: update photo card view

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,6 +1,7 @@
 import { PhotoInfo } from "@/types";
 import { NextPage } from "next";
 import Image from "next/image";
+import InfoSpan from "./InfoSpan";
 
 interface PhotoCardProps {
   photoInfo: PhotoInfo;
@@ -10,11 +11,17 @@ const PhotoCard: NextPage<PhotoCardProps> = (props) => {
   const { photoInfo } = props;
   return (
     <div className="flex w-full h-full border-2">
-      <div className="flex justify-center items-center shadow-sm">
-        <div className="flex-col w-full h-full">
-          <div className="w-full h-full p-2">
-            <Image src={photoInfo.thumbnailUrl} alt={photoInfo.title} />
-          </div>
+      <div className="flex-col w-full p-4">
+        <div className="flex w-full justify-around items-center">
+          <Image
+            src={photoInfo.url}
+            alt={photoInfo.title}
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className="w-full">
+          <InfoSpan tag="title" value={photoInfo.title} />
         </div>
       </div>
     </div>
