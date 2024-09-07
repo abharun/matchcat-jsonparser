37a69584eaff38e94022789fc59fdb32deea3d51
ABH <155776685+abharun@users.noreply.github.com>
2024-09-07 17:57:47 +0200

    feat: add user api

---
+++ src/api/user.ts
@@ -0,0 +1,23 @@
+import { JSON_PLACE_HOLDER_URL } from "@/consts";
+import { UserInfo } from "@/types/resourceInfo";
+import axios from "axios";
+
+export const fetchUsers = async () => {
+  const response = await axios.get(`${JSON_PLACE_HOLDER_URL}/users`);
+  return response.data;
+};
+
+export const createUser = async (user: UserInfo) => {
+    const response = await axios.post(`${JSON_PLACE_HOLDER_URL}/users`, user);
+    return response.data;
+}
+
+export const editUser = async (id: number, user: UserInfo) => {
+    const response = await axios.put(`${JSON_PLACE_HOLDER_URL}/users/${id}`, user);
+    return response.data;
+}
+
+export const deleteUser = async (id: number) => {
+    const response = await axios.delete(`${JSON_PLACE_HOLDER_URL}/users/${id}`);
+    return response.data;
+}
