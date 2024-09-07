a2533dca4f31b8ca6c2cabff8c868121dd790996
ABH <155776685+abharun@users.noreply.github.com>
2024-09-07 20:29:49 +0200

    fix: fix title for resource view

--- src/app/resources/layout.tsx
+++ src/app/resources/layout.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+"use client";
+
+import _ from "lodash";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function ResourceLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const path = usePathname();
+  const [title, setTitle] = useState<string>("");
+  useEffect(() => {
+    const segs = path.split("/");
+    setTitle(_.capitalize(segs[segs.length - 1]));
+  }, [path]);
+
   return (
     <div className="flex flex-col w-full h-full">
       <div className="flex w-full h-1/6 items-center">
-        <span className="text-2xl font-bold p-8">Comments</span>
+        <span className="text-2xl font-bold p-8">{title}</span>
       </div>
       <div className="flex-grow">
         <div className="flex w-full h-full justify-center items-center p-8">
